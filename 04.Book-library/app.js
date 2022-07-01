c707cfae47391180e4dfa54722027ce65902fe37
DiyanPv <105989006+DiyanPv@users.noreply.github.com>
2022-07-01 12:52:51 +0300

    JS file updated

--- 04.Book-library/app.js
+++ 04.Book-library/app.js
@@ -1,36 +1,75 @@
 const loadBooksButton = document.querySelector(`#loadBooks`);
-const tbody = document.querySelector(`tbody`)
+const tbody = document.querySelector(`tbody`);
 const url = `http://localhost:3030/jsonstore/collections/books`;
+const form = document.querySelector(`form`);
+const submitBtn = document.querySelector(`form button`)
+
 loadBooksButton.addEventListener(`click`, async (e) => {
     const initialRes = await fetch(url);
     const res = await initialRes.json();
+
     let [titleInput, authorInput] = form.querySelectorAll(`input`)
     let tds = tbody.querySelectorAll(`tr td`);
 
-    Object.values(res).forEach(el => {
+    Object.entries(res).forEach(el => {
         Array.from(tds).forEach(ell => {
-            console.log(el);
-            if (String(ell.textContent) == String(el.author) || ell.textContent == el.title) {
-                throw new Error(`Item is already in collection`)
-            }
 
 
         })
+        console.log(el);
         let tr = document.createElement(`tr`);
-        tr.innerHTML = `<td>${el.title}</td>
-                <td>${el.author}</td >
-                <td><button>Edit</button><button>Delete</button></td>`
-        tbody.appendChild(tr)
+        let id = el[0]
+
+        tr.innerHTML = `<td>${el[1].title}</td>
+                <td>${el[1].author}</td >
+                <td><button class ="editbtn" id="${id}">Edit</button><button class="deletebtn"  id="${id}">Delete</button></td>`
+        tbody.appendChild(tr);
 
     }
 
-
     )
+    const editbtns = document.querySelectorAll(`.editbtn`);
+    const deletebuttons = document.querySelectorAll(`.deletebtn`);
+    editbtns.forEach(el => el.addEventListener(`click`, (e) => {
+        let currentID = e.target.getAttribute(`id`);
+        postData = {
+            author: authorInput.value,
+            title: titleInput.value
+        }
+        let options = {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postData)
+        };
+        submitBtn.textContent = `Edit`
+        submitBtn.addEventListener(`click`, async (e) => {
+            if (authorInput.value != '' && titleInput.value != '') {
+
+                const initialRes = await fetch(`${url}/${currentID}`, options);
+                submitBtn.textContent = `Submit`
+            }
+        })
+
+    }))
+
+    deletebuttons.forEach(el => el.addEventListener(`click`, async (e) => {
+        let currentID = e.target.getAttribute(`id`);
+        console.log(currentID);
+        let options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        const initialRes = await fetch(`${url}/${currentID}`, options);
+        const res = await initialRes.json();
+    }))
 
 })
 
-const form = document.querySelector(`form`);
-const submitBtn = document.querySelector(`form button`)
+
 
 submitBtn.addEventListener(`click`, async (e) => {
     e.preventDefault()
@@ -39,6 +78,7 @@ submitBtn.addEventListener(`click`, async (e) => {
     Array.from(tds).forEach(el => {
 
         if (String(el.textContent) == String(titleInput.value) || el.textContent == authorInput.value) {
+
             throw new Error(`Item is already in collection`)
         }
 
@@ -67,3 +107,5 @@ submitBtn.addEventListener(`click`, async (e) => {
 
 
 })
+
+

81480e7933655e1c1a7707c108f5c88f47bcdcd2
Diyan Yanev <105989006+DiyanPv@users.noreply.github.com>
2022-07-23 19:28:20 +0300

    01. Furnitures Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import page from "./node_modules/page/page.mjs"
+import * as api from "./api/api.js"
+import * as data from "./data.js"
+import { dashboardCreate } from "./views/dashboard.js"
+import { editPage } from "./views/edit.js"
+import { createPage } from "./views/create.js"
+import { loginCreate } from "./views/login.js"
+import { catalogueCreate } from "./views/myfurniture.js"
+import { getFurniture } from "./data.js"
+import { html, render } from "./node_modules/lit-html/lit-html.js"
+import { registerPage } from "./views/register.js"
+window.api = api;
+window.data = data;
+
+page(`/`, dashboardCreate)
+page(`/dashboard`, dashboardCreate)
+page(`/create`, createPage)
+page(`/edit`, editPage)
+page(`/login`, loginCreate)
+page(`/my-furniture`, catalogueCreate)
+page(`/register`, registerPage)
+page(`/logout`, registerPage)
+page.start()
